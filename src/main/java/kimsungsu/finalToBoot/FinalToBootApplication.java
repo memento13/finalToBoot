d13fa03290528002138c9a128fffce051e5dbb9d
memento13 <61815697+memento13@users.noreply.github.com>
2022-02-09 20:54:57 +0900

    Initial commit

---
+++ src/main/java/kimsungsu/finalToBoot/FinalToBootApplication.java
@@ -0,0 +1,13 @@
+package kimsungsu.finalToBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalToBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalToBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimsungsu/finalToBoot/FinalToBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kimsungsu.finalToBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalToBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
